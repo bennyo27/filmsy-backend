a553d6957c88405e11308f37d9f98096091be3d0
benny <oseguerabenny27@gmail.com>
2018-12-18 18:43:04 -0800

    First commit

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ server.put("/users/:email/movie/:id", (req, res) => {
 });
 
 // server port
-const port = 3300;
+const port = process.env.PORT || 3300;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
