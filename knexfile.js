bc076d4a550966ae2cee80d8b8125c6236e508ae
benny <oseguerabenny27@gmail.com>
2019-01-09 09:26:09 -0800

    Pushing again for database#4

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,19 @@
 require('dotenv').config
 
 module.exports = {
-  // development: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: "./data/db.sqlite3"
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: "./data/migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds"
-  //   }
-  // },
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/db.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 
   production: {
     client: "pg",
